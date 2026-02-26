7766019c9c357241f5e9ebda5e5e9ac78b1a9259
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-02-26 17:05:28 +0800

    Modified Files: Project level urls.py book_club models.py, urls.py, views.py
    New Files:
    book_detail.html
    book_list.html
    migrations of the models.py
    
    Note:
    I will try to find ways on how to make migrations with on_delete=models.SetNull()
    I believe there is something else in the models that I have to do/make/edit.
    I will create an admin panel soon.

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name',]
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField()
+    publication_year = models.IntegerField()
+    created_on = models.DateTimeField()
+    updated_on = models.DateTimeField()
+
+    bookcategory = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        related_name='genres',
+    )
+
+    def __str__(self):
+        return f"{self.title} by {self.author} published in {self.publication_year} of the genre {self.bookcategory.name}."
+    
+    def get_absolute_url(self):
+        return reverse("bookclub:book_detail", args=[str(self.id)])
+
+    class Meta:
+        ordering = ['-publication_year',]
--- bookclub/views.py
+++ bookclub/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
+from .models import Book
 # Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
