232354909d9d85f3b0e4ac6517ea430f4c72f311
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-02-26 16:01:24 +0800

    It's a surprise tool that will help us later

--- commissions/views.py
+++ commissions/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+from django.http import HttpResponse
+
+from .models import Commissions
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+def index(request):
+    return HttpResponse('Sorry bro.')
+
+def commission_list(request):
+    commissions = Commissions.objects.all()
+    context = {"commissions": commissions}
+    return render(request, "ledger/commission_list.html", context)
+
+
+def commission_detail(request, pk):
+    commissions = Commission.objects.get(pk=pk)
+    context = {"recipe": recipe}
+    return render(request, "ledger/commission_detail.html", context)
