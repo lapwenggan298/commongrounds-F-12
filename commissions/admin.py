88485fe0f5c31281ccb7bfe09a5d50b7af4db689
jurvickdietrichtan <jurvick.dietrich.tan@student.ateneo.edu>
2026-02-27 20:43:05 +0800

    added admin

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Commission, CommissionType
 
-# Register your models here.
+
+@admin.register(CommissionType)
+class CommissionTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Commission)
+class CommissionAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "people_required",
+        "created_on",
+        "updated_on",
+    )
+    search_fields = ("title",)
+    list_filter = ("created_on",)
