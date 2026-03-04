ac5e10c2fe28747df64b83435dc62d3b6e3e5820
Raizon Asis <raizon@Raizons-MacBook-Pro.local>
2026-03-04 16:25:09 +0800

    Everything in the detailed description has been implemented for localevents

--- localevents/models.py
+++ localevents/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
+
+
+
+class EventType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    category = models.ForeignKey(
+        EventType, on_delete=models.SET_NULL,
+        null = True, 
+        related_name = "events")
+    
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time =models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add = True)
+    updated_on = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        ordering = ['-created_on']
+        
+    
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
