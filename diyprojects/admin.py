82e7def3a450177777aa2429e565a007fc45cfea
jdtcalanno <joshua.dame.calanno@student.ateneo.edu>
2026-03-05 23:00:58 +0800

    Added admin implementation

--- diyprojects/admin.py
+++ diyprojects/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Project, ProjectCategory
 
-# Register your models here.
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    model = ProjectCategory
+    inline = [ProjectInline]
+    list_display = ("name", "description", )
+    ordering = ("name", )
+    search_fields = ("name", )
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = (
+        "title", 
+        "category", 
+        "description", 
+        "materials", 
+        "steps", 
+        "created_on", 
+        "updated_on", 
+    )
+    search_fields = ("title", )
+    list_filter = ("created_on", "category", )
