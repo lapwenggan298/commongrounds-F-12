1974fe91cf12d5278aebeaaa957b5ae7e82130d5
jdtcalanno <joshua.dame.calanno@student.ateneo.edu>
2026-03-05 22:47:28 +0800

    Updated urls

--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
+from .views import ProjectListView, ProjectDetailView
 
 urlpatterns = [
     path(
         "projects/", 
-
+        ProjectListView.as_view(), 
+        name="project_list", 
     ), 
     path(
         "project/<int:pk>", 
-
+        ProjectDetailView.as_view(), 
+        name="project_detail", 
     ), 
 ]
 
