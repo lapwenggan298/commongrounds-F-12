e2eb0aeaa7258bf6741162002404f5c3a7e46882
Raizon Asis <raizon@Raizons-MacBook-Pro.local>
2026-03-05 21:57:09 +0800

    Added templates and edited admin on localevents

--- localevents/admin.py
+++ localevents/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Event, EventType
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    ordering = ('name', )
+    search_fields = ('name', )
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category'
+        'description'
+        'location'
+    )
+    search_fields = (
+        'title',
+        'location',
+        'description'
+    )
+
+
+
 
 # Register your models here.
