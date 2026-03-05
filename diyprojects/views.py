49487b6836186d9585a01b531482e81a84b43329
jdtcalanno <joshua.dame.calanno@student.ateneo.edu>
2026-03-05 22:40:59 +0800

    Initial views

--- diyprojects/views.py
+++ diyprojects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Project
 
-# Create your views here.
+class ProjectListView(ListView):
+    model = Project
+    template_name = "diyprojects/"
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = "diyprojects/"
