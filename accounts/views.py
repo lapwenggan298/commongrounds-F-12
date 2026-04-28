00486eb0a91d997a1f3635d7873f5efd0cc92cb3
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-04-29 00:41:07 +0800

    New branch New app, accounts, and modified settings.py
    Proj-Level
    settings.py
        - added accounts to the list of installed_apps
    
    App-Level (accounts)
    models.py
        - Profile model that is an extension of User
        - has display_name CharField, and email_address EmailField
    
    forms.py
        - ProfileUpdateForm
        - model is Profile
        - Only modifiable/updatable field is display_name
    
    views.py
        - ProfileUpdateView
        - model is Profile, form is ProfileUpdateForm, template used is profile_update.html
        - has LoginRequiredMixin for the updating to only be accessible to those logged in.
    
    profile_update.html
        - html for updating the display_name of the Profile model.
        - uses the ProfileUpdateForm.
    
    Missing/Not yet added from Requirements, Miscellaneous:
        - urls.py for accounts reflecting '/<username>/' (or the urls.py in general of the accounts app)
        - Decorator/Mixin Patterns (I forgot what this is, aside from the LoginRequiredMixin, and I can't even explain what it is)
        - The other advanced requirements (I do not understand them)
        - Other Authorization or Registration things related to authorization or logging in like the account set up, and password related htmls.

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from .models import Profile
+
+from .forms import ProfileUpdateForm
+# Create your views here.
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileUpdateForm
+    template_name = 'profile_update.html'
