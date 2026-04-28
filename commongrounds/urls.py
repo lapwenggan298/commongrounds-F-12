f06685083a14ea08be824f00d93c5c6a3e4a945d
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-04-28 22:44:30 +0800

    Modified project level urls.py
    Added 'accounts' for user management, someone will add the htmls and other things necessary for the registration/user management.

--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('bookclub/', include('bookclub.urls', namespace='bookclub')),
     path('diyprojects/', include('diyprojects.urls', namespace='diyprojects')),
     path('commissions/', include('commissions.urls', namespace='commissions')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
