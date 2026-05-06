1fd2e86bc11ea2dad620e16e025b1a56464365f5
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-05-06 16:03:19 +0800

    Another Accounts Patch     Views.py         - added the ProfileDashboardView
        Urls.py
            - added the new ProfileDashboardView
    
        Profile_Dashboard.html
            - html for viewing the user's contributions, and stuff relating to them in the commongrounds.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProfileUpdateView
+from .views import ProfileUpdateView, ProfileDashboardView
 
 urlpatterns = [
     path('<str:username>/', ProfileUpdateView.as_view(), name='profile_update'),
+    path('dashboard/', ProfileDashboardView.as_view(), name='profile_dashboard'),
 ]
 
 app_name = "accounts"
