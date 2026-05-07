8042e106253c06348b6ddfae40779ce1e22009ae
jdtcalanno <joshua.dame.calanno@student.ateneo.edu>
2026-05-07 19:30:27 +0800

    Implementation of the creation, deletion, and updating of favorites, ratings, and reviews

--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import ProjectListView, ProjectDetailView
 
 urlpatterns = [
@@ -11,6 +12,21 @@ urlpatterns = [
         "project/<int:pk>", 
         ProjectDetailView.as_view(), 
         name="project_detail"
+    ), 
+    path(
+        "project/<int:pk>/favorite",
+        views.add_favorite, 
+        name="add_favorite"
+    ), 
+    path(
+        "project/<int:pk>/rate/", 
+        views.add_rating, 
+        name="add_rating"
+    ), 
+    path(
+        "project/<int:pk>/review/", 
+        views.add_review, 
+        name="add_review"
     ), 
 ]
 
--- diyprojects/views.py
+++ diyprojects/views.py
@@ -1,8 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.db.models import Avg
 from .models import Project, Favorite, ProjectReview, ProjectRating
+
+
+@login_required
+def add_favorite(request, pk):
+        project = get_object_or_404(Project, pk=pk)
+        profile = request.user.profile
+
+        favorite = Favorite.objects.filter(
+            profile=profile,
+            project=project
+        )
+
+        if favorite.exists():
+            favorite.delete()
+        else:
+            Favorite.objects.create(
+                profile=profile,
+                project=project
+            )
+
+        return redirect("diyprojects:project_detail", pk=pk)
+
+@login_required
+def add_rating(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    profile = request.user.profile
+
+    if request.method == "POST":
+        score = request.POST.get("score")
+
+        ProjectRating.objects.update_or_create(
+            profile=profile,
+            project=project,
+            defaults={
+                "score": score
+            }
+        )
+
+    return redirect("diyprojects:project_detail", pk=pk)
+
+@login_required
+def add_review(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    profile = request.user.profile
+
+    if request.method == "POST":
+        text = request.POST.get("text")
+
+        if text:
+            ProjectReview.objects.create(
+                project=project,
+                reviewer=profile,
+                comment=text
+            )
+
+    return redirect("diyprojects:project_detail", pk=pk)
 
 class ProjectListView(ListView):
     model = Project
@@ -54,4 +111,15 @@ class ProjectDetailView(DetailView):
             ).count()
         context['favorite_count'] = favorite_count
 
+        if self.request.user.is_authenticated:
+            profile = self.request.user.profile
+
+            is_favorited = Favorite.objects.filter(
+                profile=profile,
+                project=self.object
+            ).exists()
+
+        context['is_favorited'] = is_favorited
+
         return context
+    
