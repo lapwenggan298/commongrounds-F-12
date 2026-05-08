a5044eef99b839986027c55ae8754a62890d6515
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-05-09 01:16:40 +0800

    Clean-up and admin     - Why the profiles app.... sure     - remmove migrations to set up new sqlite table.
        Admin.py
            - added RoleAdmin and RoleInLine
            - so we can add Roles via admin instead of the shell, but both ways work anyway.

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile, Role
 # Register your models here.
 
 class ProfileInLine(admin.StackedInline):
@@ -14,8 +14,17 @@ class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInLine]
 
 
+class RoleAdmin(admin.ModelAdmin):
+    model = Role
+
+
+class RoleInLine(admin.TabularInline):
+    model = Role
+
+
 class ProfileAdmin(admin.ModelAdmin):
     model = Profile
+    inlines = [RoleInLine]
     list_display = ('user', 'display_name', 'role',)
     list_filter = ('role',)
     search_fields = ('user__username', 'display_name',)
@@ -24,3 +33,4 @@ class ProfileAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Role, RoleAdmin)
