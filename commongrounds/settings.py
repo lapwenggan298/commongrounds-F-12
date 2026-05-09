06bd591a0de747ad26311238698d85fca7efd52d
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-05-10 03:17:44 +0800

    CSRF Trusted Origin Correction     - there isn't supposed to be a slash at the end?

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://commongrounds-f-12-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://commongrounds-f-12-production.up.railway.app']
 
 # Application definition
 
